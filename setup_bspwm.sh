11e89837293dae1c37272faa6050ebc6310cc1ff
vbth7777 <vbth7777@gmail.com>
2023-12-03 20:45:48 +0700

    03/12/2023

--- setup_bspwm.sh
+++ setup_bspwm.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 cd ~/.config/
 # Configs
-sudo pacman -S neovim fish alacritty polybar mpv picom
 rm -rf bspwm nvim fish picom polybar alacritty mpv
-git clone https://github.com/vbth7777/my-bspwm-config
+sudo pacman -S neovim fish alacritty polybar mpv picom
+git clone https://github.com/vbth7777/bspwm-configure bspwm
 git clone https://github.com/vbth7777/my-fish-config
-git clone https://github.com/vbth7777/my-picom-config
-git clone https://github.com/vbth7777/my-polybar-config
-git clone https://github.com/vbth7777/my-alacritty-config
-git clone https://github.com/vbth7777/astronvim-configure
-git clone https://github.com/vbth7777/mpv-configure
-mv my-bspwm-config bspwm
-mv astronvim-configure nvim
-mv my-fish-config fish
-mv my-picom-config picom
-mv my-polybar-config polybar
-mv my-alacritty-config alacritty
-mv mpv-configure mpv
+git clone https://github.com/vbth7777/my-alacritty-config alacritty
+git clone https://github.com/vbth7777/astronvim-configure nvim
+git clone https://github.com/vbth7777/mpv-configure mpv
+# git clone https://github.com/vbth7777/my-bspwm-config bspwm
+# git clone https://github.com/vbth7777/my-picom-config
+# git clone https://github.com/vbth7777/my-polybar-config
+# mv my-bspwm-config bspwm
+# mv astronvim-configure nvim
+# mv my-fish-config fish
+# mv my-picom-config picom
+# mv my-polybar-config polybar
+# mv my-alacritty-config alacritty
+# mv mpv-configure mpv
 # Dependencies
-git clone https://github.com/vbth7777/private-files
+# git clone https://github.com/vbth7777/private-files
