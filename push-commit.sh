49a20e1e46d678a452a88c869a5abf4209bfeba8
vbth7777 <vbth7777@gmail.com>
2025-08-09 23:17:42 +0700

    09/08/2025

--- push-commit.sh
+++ push-commit.sh
@@ -7,7 +7,7 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 COMMIT_MESSAGE=$(date +%d/%m/%Y)
 
 # Add all changes to the Git staging area
-git add -A
+git add .
 
 # Commit changes with the commit message
 git commit -m "$COMMIT_MESSAGE"
